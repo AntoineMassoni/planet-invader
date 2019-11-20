2c7d498cfc52e7d745c23e35f959462ad116abad
Antoine Massoni <a.massoni93@gmail.com>
2019-11-20 10:34:54 +0100

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module PlanetInvader
     config.assets.initialize_on_precompile = false
 
 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
