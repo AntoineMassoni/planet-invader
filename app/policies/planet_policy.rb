cd6b80caf6b33ab835e67b4566a322d3823f597b
Nicolas Hild <hild_nicolas@yahoo.fr>
2019-11-21 17:38:03 +0100

    authorization added

---
+++ app/policies/planet_policy.rb
@@ -0,0 +1,35 @@
+class PlanetPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def home?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
