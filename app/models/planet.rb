e946379be9e7915ba0a0e9fdfc05bf4ed65b6bc6
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-21 13:52:24 +0100

    simplified db

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -7,7 +7,7 @@ class Planet < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: true
   validates :capacity, presence: true, numericality: true
-  validates :description, :stellar_coordinates, :activities, :weather, :local_population, presence: true
+  validates :description, :stellar_coordinates, :weather, presence: true
 
   def next_bookings
     Booking.where("planet_id = ? AND check_out > ?", self.id, Date.today)
